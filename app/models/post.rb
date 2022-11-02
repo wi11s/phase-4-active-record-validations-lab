931fe4fa9e41ff4b29299439a552418eed00e3bf
William <wms274@nyu.edu>
2022-11-02 11:14:51 -0400

    commit tree 1990a962fca60c5aba6dcdc8407ddbe0b0ad38b8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :clickbait
+
+    def clickbait
+        if title.present? && !( title.match?("Won't Believe") || title.match?("Secret") || title.match?("Top") || title.match?("Guess") )
+            errors.add(:title, "not sufficiently clickbait-y")
+        end
+    end
 end
